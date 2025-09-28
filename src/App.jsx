0a73135ffa8305eb03081baf0ff64cc95f7f8ba7
saynis <aishaabdifetah@gmail.com>
2025-09-28 10:14:05 +0300

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen flex-col '>
+      <h1 className='text-2xl font-bold'>Hello my Team Local Heroes </h1>
+      <p>Welcome to our React application!</p>
+      <p>let us build </p>
+    </div>
+  )
+}
+
+export default App
