56072b7f63e91cf034dc29e84e10ee1d6d31e031
tayokod <tayokoddevelopers@gmail.com>
2025-09-29 14:43:26 +0300

    router updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Heroes from "./pages/Heroes";
+import Nominate from "./pages/Nominate";
 
 const App = () => {
   return (
-    <div className='flex justify-center items-center h-screen flex-col '>
-      <h1 className='text-2xl font-bold'>Hello my Team Local Heroes </h1>
-      <p>Welcome to our React application!</p>
-      <p>let us build our website</p>
-    </div>
-  )
-}
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="container mx-auto px-4 py-8">
+  
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            {/* <Route path="/register" element={<Register />} /> */}
 
-export default App
+            <Route path="/heroes" element={<Heroes/>} />
+            <Route path="/nominate" element={<Nominate/>} />
+          </Routes>
+      </main>
+      {/* <Footer /> */}
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
